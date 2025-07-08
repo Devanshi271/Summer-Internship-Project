8f86a613e8d22e8c9918d88bd18dcc7bb364e6e9
Devanshi271 <devanshimandalia221@gmail.com>
2025-07-08 22:54:15 +0530

    commit

---
+++ Day2_Task/LoginPage/src/app/profile/profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-profile',
+    template: `<h2>Profile Component</h2>`,
+    styles: 'h2{color: orange; border:1px solid orange}'
+})
+
+export class Profile{
+
+}
