7f83d667679c0c2b343ba8bb54cf66227947454e
Devanshi271 <devanshimandalia221@gmail.com>
2025-07-12 12:09:03 +0530

    initial commit

--- Mission_Project/MissionBackend/Mission/Mission.Services/obj/Debug/net8.0/Mission.Services.AssemblyInfo.cs
+++ Mission_Project/MissionBackend/Mission/Mission.Services/obj/Debug/net8.0/Mission.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3378a9f44b3e018265c747c3bb6df5659026a87c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
